a67ac89660b8b68122fbf6d8bd578a70a1769e77
Edgard Barbosa <edgard.barbosa@cortex-intelligence.com>
2018-10-30 19:52:32 -0300

    refactoring index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('express-async-errors');
+const logger = require('./utils/logger');
 const express = require('express');
 const app = express();
 
@@ -7,4 +8,4 @@ require('./startup/db')();
 require('./startup/config')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => logger.log('info', `Listening on port ${port}...`));
